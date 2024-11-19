9f33237d36690928f78ccf84945bc1b9b4b9bf91
Abby Fowler <agfowler@udel.edu>
2024-11-19 16:23:06 -0500

    basic window

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb
+
+root = tb.Window(themename='darkly')
+root.title("ReviewGUI")
+root.iconbitmap('favicon.png')
+root.geometry('500x350')
+
+root.mainloop()
